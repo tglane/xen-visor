6aded86f25b2cdd660a4aee20702f6235c083c37
Timo Glane <timo.glane@gmail.com>
2019-11-26 22:46:31 +0100

    first try on using libxenstore

--- tools/xen_ressource_manager/xen_ressource_manager.c
+++ tools/xen_ressource_manager/xen_ressource_manager.c
@@ -1,7 +1,20 @@
-
+#include <stdlib.h>
 #include <stdio.h>
+
+//#include <_paths.h>
+#include <xenstore.h>
 
 int main(void)
 {
-    printf("Hello World from a Xen Tool!\n");
+    struct xs_handle* xsh;
+    char* mem_load;
+    unsigned int read_length;
+    xsh = (struct xs_handle*) xs_open(0);
+
+    mem_load = (char*) xs_read(xsh, XBT_NULL, "/local/domain/0/data/memload", &read_length);
+
+    printf("Memload: %d", *mem_load);
+
+    free(mem_load);
+    xs_close(xsh);
 }
