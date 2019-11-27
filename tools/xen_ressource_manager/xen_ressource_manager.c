77497878622cc5116a624bcbf09bbf73a2c86ffc
timog <timo.glane@gmail.com>
2019-11-27 12:15:04 +0100

    read memload and cpuload from xenstore

--- tools/xen_ressource_manager/xen_ressource_manager.c
+++ tools/xen_ressource_manager/xen_ressource_manager.c
@@ -7,19 +7,39 @@
 int main(void)
 {
     struct xs_handle* xsh;
-    char* mem_load;
+    //struct expanding_buffer ebuf;
+    char* read_val;
     unsigned int read_length;
+
+    int mem_load;
+    double cpu_load;
+
     xsh = (struct xs_handle*) xs_open(0);
     if(xsh == NULL)
     {
         printf("Could not open xs_handle\n");
         return -1;
     }
+    printf("xs_handle opened\n");
 
-    mem_load = (char*) xs_read(xsh, XBT_NULL, "/local/domain/0/data/memload", &read_length);
+    read_val = xs_read(xsh, XBT_NULL, "/local/domain/0/data/memload", &read_length);
+    if(read_val == NULL)
+    {
+        printf("Could not read path\n");
+        return -1;
+    }
+    mem_load = atoi(read_val);
 
-    printf("Memload: %d", *mem_load);
+    read_val = xs_read(xsh, XBT_NULL, "/local/domain/0/data/cpuload", &read_length);
+    if(read_val == NULL)
+    {
+        printf("Could not read path\n");
+        return -1;
+    }
+    cpu_load = atof(read_val);
 
-    free(mem_load);
+    printf("Memload: %d, CPU load: %f\n", mem_load, cpu_load);
+
+    free(read_val);
     xs_close(xsh);
 }
