a445af3d447c6dad950b1128e8275bae8c8f2434
timog <timo.glane@gmail.com>
2019-12-26 22:01:43 +0100

    fixed issue where only first domain was placedon physical cpus --- still not completely tested

--- tools/xen_ressource_manager/rm_numa_manager.c
+++ tools/xen_ressource_manager/rm_numa_manager.c
@@ -3,6 +3,7 @@
 #include <syslog.h>
 
 struct numa_node_info {
+    int total_cpus;
     int num_cpus;
     int num_free_cpus;
     int* free_cpu_id;
@@ -12,6 +13,7 @@ struct numa_node_info {
 typedef struct numa_node_info numa_node_info_t;
 
 static numa_node_info_t* node_info;
+static int node_info_num_nodes;
 
 static int compare_domains_by_vcpucount(const void* a, const void* b)
 {
@@ -39,7 +41,10 @@ static int get_free_pcpu(int node_id)
     // TODO
 
     free_cpus = node_info[node_id].free_cpu_id;
-    for(i = 0; i < node_info[node_id].num_cpus; i++)
+    for(i = 0; i < node_info[node_id].total_cpus; i++)
+        syslog(LOG_NOTICE, "[DEBUG get_free_pcpu] free_cpus[%d] = %d\n", i, free_cpus[i]);
+
+    for(i = 0; i < node_info[node_id].total_cpus; i++)
     {
         if(free_cpus[i] > 0)
         {
@@ -47,28 +52,37 @@ static int get_free_pcpu(int node_id)
             return i;
         }
     }
-
+    syslog(LOG_NOTICE, "[DEBUG] get_free_pcpu ERROR\n");
     return -1;
 }
 
 int RM_NUMA_MANAGER_init_numa_info(void)
 {
-    int i, num_cpu;
+    int i, num_cpu, num_nodes;
     libxl_physinfo* phys_info = RM_XL_get_physinfo();
     libxl_cputopology* cpu_top = RM_XL_get_cpu_topology(&num_cpu);
+    libxl_numainfo* numa_info = RM_XL_get_numa_topology(&num_nodes);
 
     if(node_info != NULL || phys_info == NULL || cpu_top == NULL)
         return -1;
 
     node_info = malloc(phys_info->nr_nodes * sizeof(numa_node_info_t));
     memset(node_info, 0, phys_info->nr_nodes * sizeof(numa_node_info_t));
+    node_info_num_nodes = num_nodes;
 
-    // TODO
     for(i = 0; i < num_cpu; i++)
     {
         node_info[cpu_top[i].node].num_cpus++;
     }
 
+    for(i = 0; i < num_nodes; i++)
+    {
+        node_info[i].total_cpus = num_cpu;
+        node_info[i].free_cpu_id = malloc(num_cpu * sizeof(int));
+        memset(node_info[i].free_cpu_id, -1, num_cpu * sizeof(int));
+    }
+
+    libxl_numainfo_list_free(numa_info, num_nodes);
     libxl_cputopology_list_free(cpu_top, num_cpu);
     free(phys_info);
     return 0;
@@ -77,7 +91,15 @@ int RM_NUMA_MANAGER_init_numa_info(void)
 void RM_NUMA_MANAGER_close(void)
 {
     if(node_info != NULL)
+    {
+        int i;
+
+        for(i = 0; i < node_info_num_nodes; i++)
+            free(node_info[i].free_cpu_id);
+
         free(node_info);
+        node_info_num_nodes = 0;
+    }
 }
 
 int RM_NUMA_MANAGER_update_vcpu_placing(libxl_dominfo* dom_list, libxl_dominfo* s_dom_list, int num_domains)
@@ -94,10 +116,9 @@ int RM_NUMA_MANAGER_update_vcpu_placing(libxl_dominfo* dom_list, libxl_dominfo* 
     for(i = 0; i < num_nodes; i++)
     {
         // TODO init node_info[i].free_cpus correctly with correct pCPU ids
-        node_info[i].num_free_cpus = node_info[i].num_cpus;
-        node_info[i].free_cpu_id = malloc(num_cpu * sizeof(int));
         memset(node_info[i].free_cpu_id, -1, num_cpu * sizeof(int));
 
+        node_info[i].num_free_cpus = node_info[i].num_cpus;
         node_info[i].size_mem = numa_info[i].size;
         node_info[i].size_free_mem = numa_info[i].size;
     }
@@ -144,6 +165,11 @@ int RM_NUMA_MANAGER_update_vcpu_placing(libxl_dominfo* dom_list, libxl_dominfo* 
 
         // Sort nodes by free cpus so the node with most free cpus comes first
         qsort(node_info, num_nodes, sizeof(numa_node_info_t), compare_nodes_by_freecpus);
+
+        for(j = 0; j < num_nodes; j++)
+        {
+            syslog(LOG_NOTICE, "[DEBUG] Sorted nodes[%d].free_cpus = %d\n", j, node_info[j].num_free_cpus);
+        }
 
         for(j = 0; j < num_nodes && vcpus_placed < s_dom_list[i].vcpu_online && mem_placed < domain_memory; j++) 
         {
@@ -223,9 +249,6 @@ int RM_NUMA_MANAGER_update_vcpu_placing(libxl_dominfo* dom_list, libxl_dominfo* 
 
         libxl_vcpuinfo_list_free(vcpu_info, num_vcpus);
     }
-
-    for(i = 0; i < num_nodes; i++)
-        free(node_info[i].free_cpu_id);
     
     libxl_cputopology_list_free(cpu_top, num_cpu);
     libxl_numainfo_list_free(numa_info, num_nodes);
