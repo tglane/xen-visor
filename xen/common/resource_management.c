1f00e42e4249a05ea5596e39b17de58b18731a6c
tglane <timo.glane@gmail.com>
2020-08-15 17:33:48 +0200

    reworked the skeleton for the resource management module

--- xen/common/resource_management.c
+++ xen/common/resource_management.c
@@ -6,56 +6,91 @@
  *
  */
 
-#include <xen/resource_manager.h>
+#include <xen/resource_management.h>
 #include <public/domctl.h>
+
 
 /* Static function declarations */
 
-static uint8_t calc_domain_cpuusage(struct domain*);
+static int32_t update_domain_cpuusage(struct domain_load*, struct domain*);
 
-static bool is_pv_domain(struct domain*);
+static int32_t update_domain_memusage(struct domain_load*, struct domain*);
+
 
 /* Function definitions */
 
-struct resource_model* get_resource_model(void)
+struct resource_model* create_resource_model(void)
 {
+    struct resource_model* rm = xmalloc(struct resource_model);
+    rm->size = 0;
+    rm->load_data = NULL;
+    return rm;
+}
+
+void free_resource_model(struct resource_model* res_model)
+{
+    if(res_model == NULL)
+        return;
+
+    xfree(res_model->load_data);
+    xfree(res_model);
+
+    res_model = NULL;
+}
+
+void update_resource_model(struct resource_model* res_model)
+{
+    struct domain* dom_list_ptr;
+
     // TODO 
+    // - get domain list as struct domain* for all existing domains
+    // - update memory load per domain
+    // - !!! Destroy entry in the resource_model when a domain is destroyed in domain.c
+
+    if(res_model == NULL || res_model->size == 0)
+        return;
+
+    /* struct domain* domain_list used from shed.h */
+    for(dom_list_ptr = domain_list; dom_list_ptr != NULL; dom_list_ptr = dom_list_ptr->next_in_list)
+    {
+        uint8_t cpu_pct, mem_pct;
+
+        cpu_pct = update_domain_cpuusage(&res_model->load_data[dom_list_ptr->domain_id], dom_list_ptr);
+        if(cpu_pct >= CPU_ADD_THRESHOLD)
+        {}
+        else if(cpu_pct <= CPU_REMOVE_THRESHOLD)
+        {}
+
+        mem_pct = update_domain_memusage(&res_model->load_data[dom_list_ptr->domain_id], dom_list_ptr);
+        if(mem_pct >= MEM_ADD_THRESHOLD)
+        {}
+        else if(mem_pct <= MEM_REMOVE_THRESHOLD)
+        {}
+    }
 }
 
-void free_resource_model(struct resource_model*)
-{
-    // TODO
-}
-
-void update_resource_model(struct resource_model* resource_model)
-{
-    // TODO 
-}
-
-static uint8_t calc_domain_cpuusage(struct domain_load* curr_load, struct domain* curr)
+static int32_t update_domain_cpuusage(struct domain_load* curr_load, struct domain* curr)
 {
     struct xen_domctl_getdomaininfo curr_info;
-    s_time_t ns_elapsed;
+    s_time_t ns_elapsed, now;
     uint8_t pct;
 
-    if(curr_load == NULL || curr == NULL || is_pv_domain(curr))
+    if(curr_load == NULL || curr == NULL || !is_pv_domain(curr))
         return NO_DATA;
 
     getdomaininfo(curr, &curr_info);
-    ns_elapsed = NOW() - curr_load->last_timestamp;
-    pct = ((curr_info.cpu_time - curr_load.last_cpu_time) * 100) / ns_elapsed;
-    curr_load.last_cpu_time = curr_info.cpu_time;
+    now = NOW();
+    ns_elapsed = now - curr_load->last_timestamp;
+    pct = ((curr_info.cpu_time - curr_load->last_cpu_time) * 100) / ns_elapsed;
+    curr_load->last_cpu_time = curr_info.cpu_time;
+    curr_load->last_timestamp = now;
+    curr_load->cpu_load = pct;
    
     return pct;
 }
 
-static bool is_pv_domain(struct domain* dom)
+static int32_t update_domain_memusage(struct domain_load* curr_load, struct domain* curr)
 {
-    return !(dom->options & XEN_DOMCTL_CDF_hvm); 
+    // TODO
+    return 0;
 }
-
-static bool is_idle_domain(struct domain* dom)
-{
-    return (dom-domain_id == IDLE_DOMAIN_ID)
-}
-
--- xen/include/xen/resource_management.h
+++ xen/include/xen/resource_management.h
@@ -9,12 +9,24 @@
 #ifndef RESOURCE_MANAGEMENT_H
 #define RESOURCE_MANAGEMENT_H
 
+#include <xen/domain.h>
 #include <xen/lib.h>
 #include <xen/time.h>
+#include <xen/sched.h>
 
 /* Defines */
-#define NO_DATA 999
+#define NO_DATA -1
 #define IDLE_DOMAIN_ID 32767
+
+#define CPU_ADD_THRESHOLD 80
+#define CPU_REMOVE_THRESHOLD 20
+
+#define MEM_ADD_THRESHOLD 90
+#define MEM_REMOVE_THRESHOLD 40
+
+
+
+/* Definitions of structs used by this module */
 
 struct domain_load {
     uint8_t domain_id;
@@ -33,9 +45,12 @@ struct resource_model {
 typedef struct resource_model resource_model_t;
 
 
-struct domain_load* get_resource_model(void);
 
-void free_resource_model(struct resource_model);
+/* Function declarations exported by this module */
+
+struct resource_model* create_resource_model(void);
+
+void free_resource_model(struct resource_model*);
 
 void update_resource_model(struct resource_model*);
 
